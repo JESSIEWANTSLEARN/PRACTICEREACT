51af8bfffbb75fd4e69df28e51f74e67d4c1662c
JESSIEWANTSLEARN <palaraojessie19@gmail.com>
2026-08-19 10:03:54 +0800

    Add Services component for the Services page

---
+++ my/src/assets/pages/Services.jsx
@@ -0,0 +1,10 @@
+function Services() {
+    return (
+        <div className="page">
+            <h1>Services</h1>
+            <p>This is the Services page.</p>
+        </div>
+    );
+}
+
+export default Services;
