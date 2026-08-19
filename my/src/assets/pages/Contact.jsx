b9b96c31c487f64243206c2e2b5bc34ae8dd4a67
JESSIEWANTSLEARN <palaraojessie19@gmail.com>
2026-08-19 10:03:48 +0800

    Add Contact component for the Contact page

---
+++ my/src/assets/pages/Contact.jsx
@@ -0,0 +1,10 @@
+function Contact() {
+    return (
+        <div className="page">
+            <h1>Contact</h1>
+            <p>This is the Contact page.</p>
+        </div>
+    );
+}
+
+export default Contact;
