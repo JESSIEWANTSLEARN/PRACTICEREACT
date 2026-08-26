fd08448add77980471d3eb37bb256d46dad0cbc3
John Jessie Palarao <palaraojessie19@gmail.com>
2026-08-26 09:08:57 +0800

    Add ProductDetail component and update routes in App component

--- my/src/App.jsx
+++ my/src/App.jsx
@@ -5,58 +5,48 @@ import Sidebar from "./assets/layout/Sidebar";
 import Footer from "./assets/layout/Footer";
 
 import Home from "./assets/pages/Home";
+import ProductDetail from "./assets/pages/ProductDetail";
 import Profile from "./assets/pages/Profile";
 import State from "./assets/pages/State";
-import Error from "./assets/pages/Error";
 import About from "./assets/pages/About";
 import Services from "./assets/pages/Services";
 import Contact from "./assets/pages/Contact";
+import Error from "./assets/pages/Error";
 
 import "./App.css";
+
 function App() {
-    return (
-        <div className="app">
+  return (
+    <div className="app">
+      <NavBar />
 
-            <NavBar />
+      <div className="app-body">
+        <Sidebar />
 
-            <div className="app-body">
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
 
-                <Sidebar />
+            <Route path="/products/:id" element={<ProductDetail />} />
 
-                <main className="main-content">
+            <Route path="/profile" element={<Profile />} />
 
-                    <Routes>
+            <Route path="/state" element={<State />} />
 
-                        <Route
-                            path="/"
-                            element={<Home />}
-                        />
+            <Route path="/about" element={<About />} />
 
-                        <Route
-                            path="/profile"
-                            element={<Profile />}
-                        />
+            <Route path="/services" element={<Services />} />
 
-                        <Route
-                            path="/state"
-                            element={<State />}
-                        />
+            <Route path="/contact" element={<Contact />} />
 
-                        <Route
-                            path="*"
-                            element={<Error />}
-                        />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </main>
+      </div>
 
-                    </Routes>
-
-                </main>
-
-            </div>
-
-            <Footer />
-
-        </div>
-    );
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
